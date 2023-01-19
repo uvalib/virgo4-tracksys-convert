260fed874852712685456145590ad71ef75d24ab
Dave Goldstein <dpg3k@virginia.edu>
2023-01-19 10:54:37 -0500

    Add call to application in startup script

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,6 +1,9 @@
 # set environment
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
+# run application
+java -cp "lib/*:lib_aws/*:dist/sqs_tracksys.jar" edu.virginia.lib.sqsserver.SQSQueueDriver --sqs-in ${VIRGO4_TRACKSYS_CONVERT_IN_QUEUE} --sqs-out ${VIRGO4_TRACKSYS_CONVERT_OUT_QUEUE} -s3 ${VIRGO4_SQS_MESSAGE_BUCKET}
+
 #
 # end of file
 #
